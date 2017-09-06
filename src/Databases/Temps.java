ad0202de02b7151e1fba4715b310d63227302beb
John Mauk <john-mauk@uiowa.edu>
2017-09-06 08:48:12 -0500

    Added remove data method

--- src/Databases/Temps.java
+++ src/Databases/Temps.java
@@ -10,6 +10,7 @@ public class Temps {
         Connection connection = null;
         try {
             connection = ExDatabase.open();
+            System.out.println("Removed Old Date: " + removeOldData(connection));
             System.out.println("Uploaded Data: " + upLoadData(connection,tempDataGenerator()));
             ExDatabase.close(connection);
         } catch (Exception e) {
@@ -24,9 +25,18 @@ public class Temps {
     private static String[][] tempDataGenerator(){
         String tempData [][] = new String [300][2];
         Random random = new Random();
+        long currentTime = System.currentTimeMillis();
 
-        for(int ii = 0; ii < 300; ii++){
-            tempData[ii][0] = new SimpleDateFormat("yyyy-MM-dd  HH.mm.ss").format(new Date(System.currentTimeMillis() + 1000*ii+ 5*3600000));
+        for(int ii = 0; ii < 100; ii++){
+            tempData[ii][0] = new SimpleDateFormat("yyyy-MM-dd  HH.mm.ss").format(new Date(currentTime + 1000*ii+ 5*3600000));
+            tempData[ii][1] = Integer.toString(random.nextInt(73)-10);
+        }
+        for(int ii = 100; ii < 200; ii++){
+            tempData[ii][0] = new SimpleDateFormat("yyyy-MM-dd  HH.mm.ss").format(new Date(currentTime + 1000*ii+ 5*3600000));
+            tempData[ii][1] = "null";
+        }
+        for(int ii = 200; ii < 300; ii++){
+            tempData[ii][0] = new SimpleDateFormat("yyyy-MM-dd  HH.mm.ss").format(new Date(currentTime + 1000*ii+ 5*3600000));
             tempData[ii][1] = Integer.toString(random.nextInt(73)-10);
         }
         System.out.println("Date: " + tempData[0][0]);
@@ -42,7 +52,7 @@ public class Temps {
             stat = con.createStatement();
             for(int ii = 0; ii < 300; ii++){
                 String sql = "INSERT into temps(temp_datetime, temp_c_val, temp_series_name) VALUES" +
-                    "('" + tempData[ii][0]+ "'," + tempData[ii][1] + ", 'testing');";
+                    "('" + tempData[ii][0]+ "'," + tempData[ii][1] + ", 'Temps');";
                 stat.executeUpdate(sql);
             }//end for
             stat.close();
@@ -52,11 +62,48 @@ public class Temps {
         catch(Exception ex){
             System.err.println("UploadData: " + ex.getClass().getName() + ": " + ex.getMessage());
             System.exit(0);
+            flag = false;
 
         }
         return flag;
 
     }//end upLoadData
+
+    private static boolean removeOldData(Connection con){
+        boolean removed = false;
+        Statement stmt = null;
+
+        try{
+            con.setAutoCommit(false);
+            stmt = con.createStatement();
+            Date currentTime = new Date(System.currentTimeMillis());
+            String sql = "Select * FROM temps WHERE temp_datetime<'"+currentTime+"';";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            int count = 0;
+
+            String sqlRemove="";
+            while(rs.next()){
+                if(count==0){
+                    sqlRemove ="DELETE FROM temps WHERE temp_id IN (";
+                    count++;
+                }else {
+                    sqlRemove = sqlRemove + rs.getString("temp_id") + ",";
+                }
+            }
+            if(count==1) {
+                sqlRemove = sqlRemove.substring(0, sqlRemove.length() - 1) + ");";
+                stmt.executeUpdate(sqlRemove);
+                stmt.close();
+            }
+            con.commit();
+            removed = true;
+        }catch(Exception ex){
+            System.err.println("Remove Old Data: " + ex.getClass().getName() + ": " + ex.getMessage());
+            System.exit(0);
+        }
+        return removed;
+    }
 
 }
 
