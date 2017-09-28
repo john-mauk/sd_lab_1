67fdebb7193b71ea238c60d97ac2d8aed40d386e
emmyM <elinmcgee@gmail.com>
2017-09-28 03:22:04 -0500

    committing something in the god damn early morning. :D

--- web/js/index.js
+++ web/js/index.js
@@ -7,37 +7,12 @@ $(function () {
 
     $("#removePhone").click(function () {
         if (confirm("Are you sure you want to remove info?")) {
-            sendContact("remove");
-            var phone = document.getElementById("contact-modal-phone").value.toString;
-            var carrier = document.getElementByID("contact-modal-carrier").value;
-
-            if (carrier !== "--Select Carrier--" && phone === "/[0-9]/" && (phone.length === 10)) {
-                document.getElementById("removeButton").disabled = false;
-            }
-            else{
-                document.getElementByID("removeButton").disabled = true;
-            }
-        }
+            sendContact("remove");}
     });
 
     $("#testPhone").click(function(){
         sendContact("test");
-        var phone = document.getElementById("contact-modal-phone").value.toString;
-        var carrier = document.getElementByID("contact-modal-carrier").value;
-
-        if (carrier !== "--Select Carrier--" && phone === "/[0-9]/" && (phone.length === 10)) {
-            document.getElementById("testPhone").disabled = false;
-        }
-        else{
-            document.getElementByID("addPhone").disabled = true;
-        }
-    })
-    $("#displayOn").click(function(){
-
-    })
-    $("#displayOff").click(function(){
-
-    })
+    });
     $("#ServiceOn").click(function(){
 
     })
@@ -63,21 +38,25 @@ function sendContact(action) {
     var info = "btn=" + action + "&";
     info += "p=" + document.getElementById("contact-modal-phone").value + "&";
     info += "c=" + document.getElementById("contact-modal-carrier").value;
-
-    $.post("Contact.AddRemoveTest", info,
-        function (msg) {
-            console.log(msg);
-            if (msg === "added") {
-                console.log("added");
-            } else if (msg === "removed") {
-                console.log("removed");
-            } else if (msg === "tested") {
-                console.log("tested");
-            } else {
-                console.log("error");
+    if(checkNum(parseInt(document.getElementById("contact-modal-phone").value))){
+        $.post("Contact.AddRemoveTest", info,
+            function (msg) {
+                console.log(msg);
+                if (msg === "added") {
+                    console.log("added");
+                } else if (msg === "removed") {
+                    console.log("removed");
+                } else if (msg === "tested") {
+                    console.log("tested");
+                } else {
+                    console.log("error");
+                }
             }
-        }
-    );
+        );
+    }//end if
+    else{
+        alert("Invalid number, must enter a numeric value");
+    }
     console.log("posted it");
 }
 function sendHigh(){
@@ -93,20 +72,30 @@ function sendLow(){
     });
 }//end sendLow
 function checkHighBounds(){
-    high = document.getElementByID("highTemp").value;
-    if(high = "/[0-9]/" && parseInt(high) >-10 && parseInt(high) <=62){
+    var high =parseInt(document.getElementByID("highTemp").value);
+    if(checkNum(high) && high >-10 && high <=62){
         console.log("high threshold temp acceptable.")
     }
     else{
         console.log("high threshold temp failed");
+        alert("Invalid High Threshold");
     }
 }//end checkHighBounds
 function checkLowBounds(){
-    low = document.getElementByID("lowTemp").value;
-    if(low = "/[0-9]/" && parseInt(low) >=-10 && parseInt(high) <62){
+    var low = parseInt(document.getElementByID("lowTemp").value);
+    if(checkNum(low) && low >=-10 && low <62){
         console.log("Temp threshold temp acceptable.")
     }
     else{
         console.log("Low threshold temp failed");
+        alert("Invalid Low Threshold");
     }
 }//end checkLowBounds
+function checkNum(value){
+    if(value==="/[0-9]+/"){
+        return true;
+    }
+    else{
+        return false;
+    }
+}//end checkNum
